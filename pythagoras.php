f6525db9e6b135a499a44749b7adeb8af2448e60
Zbynek Dalecky <daleckyz@gmail.com>
2019-10-23 20:58:15 -0700

    obsah rovnostranneho trojuhelnika

--- pythagoras.php
+++ pythagoras.php
@@ -2,8 +2,18 @@
 $a=2;
 $b=3;
 $obdelnikobsah=($a * $b);
+$strana=6;
+$uhel=60;
+$vyska=($strana * sin(deg2rad($uhel)));
+$trojuhelnikobsah=round(($strana * $vyska / 2));
 
-echo ('Obsah obdelniku o stranach: '.$a.'cm, '.$b.'cm je '.$obdelnikobsah.'cm2.');
+echo ('Obsah obdelniku o stranach: '.$a.'cm, '.$b.'cm je '.$obdelnikobsah.'cm2.'); ?>
+<br>
+<br>
+
+<?php
+echo ('Rovnostranný trojúhelník o straně délky '.$strana.'cm a uhlu '.$uhel.'stupnu je '.$trojuhelnikobsah.'cm2.');
+
 
 ?>
 
